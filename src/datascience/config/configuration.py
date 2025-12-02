daf909ebafeb7fcc5aa6d6c48fa79b36e6d8f08c
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-12-02 11:22:24 +0545

    add data validation pipeline

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,4 +1,4 @@
-from src.datascience.entity.config_entity import DataIngestionConfig
+from src.datascience.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 
@@ -21,3 +21,17 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+        
+        create_directories([config.root_dir])
+        
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            status_file=config.status_file,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+        return data_validation_config
--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -8,3 +8,9 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
     
+@dataclass
+class DataValidationConfig:
+    root_dir: Path
+    status_file: str
+    unzip_data_dir: Path
+    all_schema: dict
