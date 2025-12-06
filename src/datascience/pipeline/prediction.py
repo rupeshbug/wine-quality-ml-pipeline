219c5c76ac04f3f3532b6af49568ca92db39a1a2
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-12-06 12:12:25 +0545

    modify prediction pipeline

--- src/datascience/pipeline/prediction.py
+++ src/datascience/pipeline/prediction.py
@@ -3,27 +3,15 @@ from pathlib import Path
 import pandas as pd
 
 class PredictionPipeline:
-    def __init__(self, best_model_path: str = "artifacts/model_trainer/best_model.joblib"):
-        self.best_model_path = Path(best_model_path)
-        if not self.best_model_path.exists():
-            raise FileNotFoundError(f"Best model not found at {self.best_model_path}")
-        
-        # Load the best model
-        self.model = joblib.load(self.best_model_path)
-        print(f"Loaded best model from {self.best_model_path}")
+    def __init__(self, model_path: str = "artifacts/model_trainer/best_model.joblib"):
+        self.model_path = Path(model_path)
+        if not self.model_path.exists():
+            raise FileNotFoundError(f"Best model not found at {self.model_path}")
+        self.model = joblib.load(self.model_path)
 
     def predict(self, data: pd.DataFrame):
         """
-        Make predictions on the input data.
-        
-        Args:
-            data (pd.DataFrame): Input features for prediction.
-        
-        Returns:
-            np.ndarray: Predictions.
+        data: pandas DataFrame with the same features used during training
+        returns: model predictions as a numpy array
         """
-        if not isinstance(data, pd.DataFrame):
-            raise ValueError("Input data should be a pandas DataFrame")
-        
-        prediction = self.model.predict(data)
-        return prediction
+        return self.model.predict(data)
